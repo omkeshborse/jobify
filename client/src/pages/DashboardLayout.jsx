4b4e5654cac44430f7b5160d0c337174bb4c6260
omkeshborse <bomkesh25@gmail.com>
2024-09-11 13:30:41 +0530

    feat Dashboard  setup

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,6 +1,11 @@
+import { Outlet } from "react-router-dom"
+
 const DashboardLayout = () => {
   return (
-    <div>DashboardLayout</div>
+    <div>
+      <Outlet />
+    </div>
+    
   )
 }
 export default DashboardLayout
