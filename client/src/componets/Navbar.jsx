24b5eaa87aff2db42167e519ddaea2d79ba651a4
omkeshborse <bomkesh25@gmail.com>
2024-09-11 22:22:36 +0530

    feat : navbar completed

--- client/src/componets/Navbar.jsx
+++ client/src/componets/Navbar.jsx
@@ -1,8 +1,24 @@
-import Wrapper from "../assets/wrappers/Navbar"
-
+import Wrapper from "../assets/wrappers/Navbar";
+import Logo from "../componets/Logo"
+import { FaAlignLeft } from "react-icons/fa";
+import { useDahsboardContext } from "../pages/DashboardLayout";
 const Navbar = () => {
+    const { toggleSideBar } = useDahsboardContext(); ;
   return (
-    <Wrapper>Navbar</Wrapper>
-  )
-}
-export default Navbar
+    <Wrapper>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSideBar}>
+            <FaAlignLeft />
+        </button>
+        <div>
+            <Logo />
+            <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">
+            Toogle/Logout
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+export default Navbar;
